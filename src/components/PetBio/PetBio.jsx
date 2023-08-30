0f754e226c30e327d665768b97820694398fbf3f
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-08-30 11:21:08 -0400

    add boilerplate to CommentSection component and update fields on PetBio

--- src/components/PetBio/PetBio.jsx
+++ src/components/PetBio/PetBio.jsx
@@ -5,20 +5,19 @@ export default function PetBio() {
   return (
     <div className="pet-bio-container">
       <div className="pet-bio-img"><img src="cutedog.com" /></div>
-      <h1>Pet Name</h1>
+      <h1>Name:</h1>
       <p>Status:</p>
+      <p>Breed:</p>
       <p>Approximate Age:</p>
-      <p>Bio</p>
+      <p>Gender:</p>
+      <p>Description:</p>
 
       <div className="buttons">
         <button>Favorite</button>
         <button>I'm interested!</button>
       </div>
 
-      <div className="comment-section">
 
       </div>
-      
-    </div>
   )
 }
