434e355bbe22271744e7e8265969257ebb399c0d
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-31 11:30:19 -0400

    display comments

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function CommentSection() {
+export default function CommentSection({ comments }) {
+  // const [comments, setComments] = useState([]);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    // setComments(post.comments);
+    // setComments(comments)
+    // setUser(JSON.parse(localStorage.getItem("user")));
+    console.log("posts: ", comments);
+  }, []);
+
   return (
-    <div>CommentSection</div>
-  )
+    <div>
+      {comments?.map((comment) => (
+        <div key={comment.id}>
+          <p>user here</p>
+          <p>{comment.text}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/pages/PetDetail/PetDetail.jsx
+++ src/pages/PetDetail/PetDetail.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import PetBio from '../../components/PetBio/PetBio'
-import CommentSection from '../../components/CommentSection/CommentSection'
+import React from "react";
+import PetBio from "../../components/PetBio/PetBio";
+import CommentSection from "../../components/CommentSection/CommentSection";
+import { useParams } from "react-router-dom";
+import { getCat } from "../../services/cats";
+import { useState, useEffect } from "react";
 
 export default function PetDetail() {
+  const [pet, setPet] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPet = async () => {
+      const pet = await getCat(id);
+      console.log("pet: ", pet);
+      setPet(pet);
+    };
+    fetchPet();
+  }, [id]);
+
   return (
     <div>
-      <PetBio />
-      <CommentSection />
+      <PetBio pet={pet} />
+      <CommentSection comments={pet.comments} />
     </div>
-  )
+  );
 }
