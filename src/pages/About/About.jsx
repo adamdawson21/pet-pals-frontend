12f71f5331c34ce424fda90505d2dc829a5395d5
Keith Lee <khlee930121@gmail.com>
2023-09-04 15:10:02 -0400

    solved problem / edit about page to place photo in the middle

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,36 +1,53 @@
-import "./About.css";
+import './About.css';
 
 export default function About() {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-
-      <h2>Our Mission</h2>
+    <div className='about'>
+      <h1>
+        <strong>Our Mission</strong>
+      </h1>
 
       <p>
-        At PetPals, our mission is to provide a compassionate and innovative platform that bridges the  gap between furry companions in need and the loving families they deserve.
-        <br /><br />
-
-        We connect potential adopters with a diverse range of adoptable pets, each with their own unique story and charm. Our comprehensive profiles empower families to make informed decisions and find a perfect match that suits their lifestyle.
-        <br /><br />
-
+        At PetPals, our mission is to provide a compassionate and innovative
+        platform that bridges the gap between furry companions in need and the
+        loving families they deserve.
+        <br />
+        <br />
+        We connect potential adopters with a diverse range of adoptable pets,
+        each with their own unique story and charm. Our comprehensive profiles
+        empower families to make informed decisions and find a perfect match
+        that suits their lifestyle.
+        <br />
+        <br />
         Join us in shaping a brighter future, one adoption story at a time.
       </p>
 
-      <img src="https://images.unsplash.com/photo-1514147397633-2882c06ea2a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2769&q=80" alt="dog" height="400px" width="650px"/>
-      
+      <img
+        src='https://images.unsplash.com/photo-1514147397633-2882c06ea2a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2769&q=80'
+        alt='dog'
+        height='400px'
+        width='650px'
+        className='about-image'
+      />
+
       <h2>Get Involved</h2>
 
       <p>
-        There are many ways to get involved with PetPals. Whether you are looking to adopt, volunteer, or donate, we are always happy to welcome new members to our community.
-        <br /><br />
-
-        As a volunteer, you'll enjoy the rewards of seeing animals transform under your care, the joy of matching pets with loving families, and the satisfaction of knowing you're making a tangible difference in the lives of vulnerable animals.
-        <br /><br />
-
-        Ready to make a difference? Contact us at info@petpals.netflify.com or visit petpals.netlify.com to learn more about our volunteer opportunities and how you can become part of our dedicated team.
+        There are many ways to get involved with PetPals. Whether you are
+        looking to adopt, volunteer, or donate, we are always happy to welcome
+        new members to our community.
+        <br />
+        <br />
+        As a volunteer, you'll enjoy the rewards of seeing animals transform
+        under your care, the joy of matching pets with loving families, and the
+        satisfaction of knowing you're making a tangible difference in the lives
+        of vulnerable animals.
+        <br />
+        <br />
+        Ready to make a difference? Contact us at info@petpals.netflify.com or
+        visit petpals.netlify.com to learn more about our volunteer
+        opportunities and how you can become part of our dedicated team.
       </p>
-
     </div>
   );
 }
