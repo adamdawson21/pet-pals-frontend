b15be115bc88df9bfc1e755f0516f1a1560b8149
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-09-05 15:56:28 -0400

    fixed typo in AboutHero

--- src/components/AboutHero/AboutHero.jsx
+++ src/components/AboutHero/AboutHero.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import "./AboutHero.css";
 
 function AboutHero() {
   return (
     <div className="hero-container">
       <img
-        className="about-image"
+        className="aboutus-image"
         src="https://imgur.com/1GM1mzR.jpg"
         alt="about us"
       />
